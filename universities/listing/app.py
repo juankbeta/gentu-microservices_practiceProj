7704d001d3a390a0c42abe4255bc392d2fb7d518
juankbeta <juan2.betancur@ucp.edu.co>
2024-08-13 00:32:50 -0400

    Adding updates to the app.py files in the website and listening directories

--- universities/listing/app.py
+++ universities/listing/app.py
@@ -25,6 +25,19 @@ def getCollegesListByName(name):
             colleges.append(college["name"])
     return json.dumps({"Colleges": colleges},indent=4)
 
+# Update the listings microservices to retrieve the list of universities in a 
+# case-insensitive manner. Verify the same passing “TriniTY”, as a parameter. If 
+# successfully updated, the endpoint should return all the universities with the word 
+# ‘Trinity’.
+
+@app.route("/colleges/<name>")
+def getCollegesListByName(name):
+     name = name.lower()  # Convert the search parameter to lowercase
+     colleges = []
+     for college in data:
+        if name in college["name"].lower(): # Convert university name to lowercase for case-insensitive comparison
+            colleges.append(college["name"])
+    return json.dumps({"Colleges": colleges},indent=4)
 
 if __name__ == "__main__":
     app.run(debug=True)
